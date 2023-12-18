3eb107b4b48047feed0ca651bd6d08fa8bdcbe50
51zzZ <jackywoo1024@qq.com>
2023-12-18 16:14:14 +0800

    wish simulator

--- app/src/main/java/com/example/gacha/MainActivity.java
+++ app/src/main/java/com/example/gacha/MainActivity.java
@@ -7,7 +7,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 抽卡网址
     private String URL = "https://wishsimulator.app/";
 
     @Override
